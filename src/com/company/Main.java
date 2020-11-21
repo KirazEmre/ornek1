200875751a8f224157f7ecdf4ed3893a74606d7a
KirazEmre <kirazemre1199@gmail.com>
2020-11-21 21:07:34 +0300

    my first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,49 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+        int a = 5;
+        int b = a;
+        System.out.println("a = > "+a);
+        System.out.println("b = > "+b);
+
+
+        /*
+        int a = 5;
+        System.out.println("a+ = > "+ a++);
+        System.out.println("+a = > "+ ++a);
+
+        int b = 5;
+        System.out.println("\n+b = > "+ ++b);
+        System.out.println("b+ = > "+ b++);
+        */
+
+        /*
+        String yas = "60";
+        int yasi = Integer.parseInt(yas);
+
+        System.out.println("yas = >"+ (yasi+1)); //parantez koymsaydik 601 geliyor
+        //System.out.println("yas = >"+ (Integer.parseInt(yas)+1);
+        System.out.println("yas = >"+ yas);
+        */
+
+        /*
+        double d = 100.04;
+        long l = (long)d;
+        int i = (int)l;
+
+
+        System.out.println("d = "+d);
+        System.out.println("l = "+l);
+        System.out.println("i = "+i);
+        */
+        /*
+        int m= 10%3;
+        System.out.println("m ="+m);
+        */
+    }
+}
